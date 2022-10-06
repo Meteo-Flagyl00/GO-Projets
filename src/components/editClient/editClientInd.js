85a32db6c85ec8702e60daf443dd0fbc69e21761
Meteo-Flagul00 <med.elkadiri48@gmail.com>
2022-10-06 15:14:01 +0100

    adding edit method code

--- src/components/editClient/editClientInd.js
+++ src/components/editClient/editClientInd.js
@@ -1,56 +1,162 @@
-import React from 'react'
-import { Grid, Paper, Button, Typography } from '@mui/material'
-import { TextField } from '@mui/material'
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from "yup";
+import React from "react";
+import { Grid, Paper, Button, Typography } from "@mui/material";
+// import { TextField } from "@mui/material";
+// import { Formik, Form, Field, ErrorMessage } from "formik";
+// import * as Yup from "yup";
+import axios from "axios";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useParams, Link, useNavigate } from "react-router-dom";
+import "./editClientind.css";
 
 const RegistrationForm = () => {
-    const paperStyle = { padding: '0 15px 0 15px', width: 400, }
-    const btnStyle = { marginTop: 10 }
-    const phoneRegExp=/^[2-9]{2}[0-9]{8}/
-    const passwordRegExp=/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/
-    const CIN= /^[A-Z][A-Z][0-9]/
-    
-    const initialValues = {
-        firstName: '',
-        lastName: '',
-        email: '', 
-        phoneNumber: '',
-        password: '',
-        confirmPassword:''
-    }
-    const validationSchema = Yup.object().shape({
-        firstName: Yup.string().min(3, "It's too short"),
+  const paperStyle = { padding: "0 15px 0 15px", width: 400 };
+  const btnStyle = { marginTop: 10, marginBottom: 10 };
+  //   const phoneRegExp = /^[2-9]{2}[0-9]{8}/;
+  //   const passwordRegExp =
+  //     /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+  //   const CIN = /^[A-Z][A-Z][0-9]/;
+ 
+  const [user, setUser] = useState({
+    FullName: "",
+    Email: "",
+    Phone: "",
+    Nationality: "",
+  });
 
-        lastName: Yup.string().min(3, "It's too short"),
+  const { id } = useParams();
 
-        email: Yup.string().email("Enter valid email"),
-        // phoneNumber: Yup.number().typeError("Enter valid Phone number").required("Required"),
-        phoneNumber:Yup.string().matches(phoneRegExp,"Enter valid Phone number"),
+  //   const { FullName, Email, Phone, Nationality } = user;
 
-        cin:Yup.string().matches(CIN,"Enter valid CIN code").required('Required'),
+  function handle(e) {
+    const newData = { ...user };
+    newData[e.target.id] = e.target.value;
+    setUser(newData);
+    console.log(newData);
+    // setUser({ ...user, [e.target.id]: e.target.value})
+  }
 
-        password: Yup.string().min(8, "Minimum characters should be 8")
-        .matches(passwordRegExp,"Password must have one upper, lower case, number, special symbol"),
+  let Navigate = useNavigate();
 
-        confirmPassword:Yup.string().oneOf([Yup.ref('password')],"Password not matches").required('Required')
-    })
-    const onSubmit = (values, props) => {
+  useEffect(() => {
+    loadUser();
+  }, []);
 
-        alert(JSON.stringify(values), null, 2)
-        props.resetForm()
-    }
-    return (
-        <Grid>
-            <Paper elevation={0} style={paperStyle}>
-                <Grid align='center'>
-                    <Typography variant='caption'>  </Typography>
-                </Grid>
-                <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
+  const onSubmit = async (e) => {
+    e.preventdefault();
+    await axios.put(`https://6336d4765327df4c43ca66a2.mockapi.io/users/${id}`, user);
+    Navigate("/");
+  };
+
+
+
+  const loadUser = async () => {
+    const result = await axios.get(`https://6336d4765327df4c43ca66a2.mockapi.io/users/${id}`);
+    setUser(result.data);
+  };
+
+  //   const initialValues = {
+
+  //   };
+  //   const validationSchema = Yup.object().shape({
+  //     firstName: Yup.string().min(3, "It's too short"),
+
+  //     lastName: Yup.string().min(3, "It's too short"),
+
+  //     email: Yup.string().email("Enter valid email"),
+  //     phoneNumber: Yup.number().typeError("Enter valid Phone number").required("Required"),
+  //     phoneNumber: Yup.string().matches(phoneRegExp, "Enter valid Phone number"),
+
+  //     cin: Yup.string().matches(CIN, "Enter valid CIN code").required("Required"),
+
+  //     password: Yup.string()
+  //       .min(8, "Minimum characters should be 8")
+  //       .matches(
+  //         passwordRegExp,
+  //         "Password must have one upper, lower case, number, special symbol"
+  //       ),
+
+  //     confirmPassword: Yup.string()
+  //       .oneOf([Yup.ref("password")], "Password not matches")
+  //       .required("Required"),
+  //   });
+
+  //update client
+
+  return (
+    <div className="formEdit">
+      <Grid>
+        <Paper elevation={1} style={paperStyle}>
+          <Grid align="center">
+            <Typography variant="caption">Client Edit</Typography>
+          </Grid>
+
+          <form
+            onSubmit={(e) => onSubmit(e)}
+            style={{ display: "flex", gap: ".5rem", flexDirection: "column" }}
+          >
+            <label>Full Name</label>
+            <input
+              className="inp"
+              placeholder="Name"
+              type="text"
+              onChange={(e) => handle(e)}
+              id="FullName"
+              value={user.FullName}
+            />
+
+            <label>Email</label>
+            <input
+              className="inp"
+              placeholder="Email"
+              type="text"
+              onChange={(e) => handle(e)}
+              id="Email"
+              value={user.Email}
+            />
+
+            <label>Phone Number</label>
+            <input
+              className="inp"
+              placeholder="Phone"
+              type="text"
+              onChange={(e) => handle(e)}
+              id="Phone"
+              value={user.Phone}
+            />
+
+            <label>Nationality</label>
+            <input
+              className="inp"
+              placeholder="Nationality"
+              type="text"
+              onChange={(e) => handle(e)}
+              id="Nationality"
+              value={user.Nationality}
+            />
+            <div className="btnEdit">
+              <Button
+                type="submit"
+                style={btnStyle}
+                variant="contained"
+                color="primary"
+              >
+                Register
+              </Button>
+
+              <Link to="/Clients">
+                <Button style={btnStyle} variant="contained" color="error">
+                  Cancel
+                </Button>
+              </Link>
+            </div>
+          </form>
+
+          {/* <Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
                     {(props) => (
                         <Form noValidate>
-                            {/* <TextField label='Name' name="name" fullWidth value={props.values.name}
-                    onChange={props.handleChange} /> */}
+                            <TextField label='Name' name="name" fullWidth value={props.values.name}
+                    onChange={props.handleChange} />
 
                             <Field as={TextField} name='firstname' label='firstName' fullWidth
                                 error={props.errors.name && props.touched.name}
@@ -60,8 +166,8 @@ const RegistrationForm = () => {
                                 error={props.errors.name && props.touched.name}
                                 helperText={<ErrorMessage name='lastname' />} required />
 
-                            {/* <TextField label='Email' name='email' type='Email' fullWidth 
-                    {...props.getFieldProps('email')}/> */}
+                            <TextField label='Email' name='email' type='Email' fullWidth 
+                    {...props.getFieldProps('email')}/>
 
                             <Field as={TextField} name='email' label='Email' fullWidth
                                 error={props.errors.email && props.touched.email}
@@ -87,10 +193,11 @@ const RegistrationForm = () => {
                                 color='primary'>Register</Button>
                         </Form>
                     )}
-                </Formik>
-            </Paper>
-        </Grid>
-    )
-}
+                </Formik> */}
+        </Paper>
+      </Grid>
+    </div>
+  );
+};
 
 export default RegistrationForm;
