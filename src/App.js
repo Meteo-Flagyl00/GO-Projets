7378f0e4151da9bf989e93ed011946c424501983
Meteo-Flagul00 <med.elkadiri48@gmail.com>
2022-10-06 15:10:13 +0100

    adding viewClient and edit page routes

--- src/App.js
+++ src/App.js
@@ -9,20 +9,24 @@ import Facture from './components/pages/facture/Facture';
 import Projects from './components/pages/projects/Projects';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NewFacture from './components/pages/NewFacture/NewFacture';
+import RegistrationForm from './components/editClient/editClientInd';
+import ViewClient from './components/pages/viewClient/ViewClient';
 
 
 function App() {
   return (
     <div className="App">
-      <div className='AppGlass'>
-      <Sidebar/>
+      <div className="AppGlass">
+        <Sidebar />
         <Routes>
-          <Route path='/' element={<MainDash/>}/>
-          <Route path='/Clients' element={<Clients/>}/> 
-          <Route path='/Facture' element={<Facture/>}/>
-          <Route path='/Projects' element={<Projects/>}/>
-          <Route path='/NewFacture' element={<NewFacture/>}/>       
-        </Routes> 
+          <Route path="/" element={<MainDash />} />
+          <Route path="/Clients" element={<Clients />} />
+          <Route path="/Clients/Client/:id" element={<RegistrationForm />} />
+          <Route path="/Facture" element={<Facture />} />
+          <Route path="/Projects" element={<Projects />} />
+          <Route path="/NewFacture" element={<NewFacture />} />
+          <Route path="/Clients/ViewClient/:id" element={<ViewClient/>}/>
+        </Routes>
       </div>
     </div>
   );
