acf0f088f82e96c3c484abf3695e8f449ef2a76f
Meteo-Flagul00 <med.elkadiri48@gmail.com>
2022-10-12 10:16:49 +0100

    changing display to grid for maindash

--- src/components/MainDash/MainDash.jsx
+++ src/components/MainDash/MainDash.jsx
@@ -6,16 +6,19 @@ import './MainDash.css'
 
 function MainDash() {
   return (
-    <div className='maindash'>
-      <h1>Dashboard</h1>
-      <Cards/>
-      <Table/>
-
-      {/* <div className='rightSide'>
-        <AboutUs/>
-      </div> */}
+    <div className="grid-container">
+      <div className="maindash">
+        <h1>Dashboard</h1>
+        <Cards />
+        <h1>Recent Clients</h1>
+        <Table />
+      </div>
+      <div className="rightSide">
+        <h1>WebSite Infos </h1>
+        <AboutUs />
+      </div>
     </div>
-  )
+  );
 }
 
 export default MainDash
