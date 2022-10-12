560409ebc17588e5ff47e16e681466859d9bf212
Meteo-Flagul00 <med.elkadiri48@gmail.com>
2022-10-12 10:10:41 +0100

    adding next and previous buttons to Pagination

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Pagination = ({ UsersPerPage, totalUsers, paginate }) => {
+  const [currentPage, setCurrentPage] = useState(0)
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalUsers / UsersPerPage); i++) {
@@ -10,6 +11,18 @@ const Pagination = ({ UsersPerPage, totalUsers, paginate }) => {
   return (
     <nav>
       <ul className="pagination">
+        {pageNumbers.includes(currentPage - 1) && (
+          <a
+            className="page-link"
+            onClick={() => {
+              setCurrentPage(currentPage - 1);
+              paginate(currentPage - 1);
+            }}
+          >
+            prev
+          </a>
+        )}
+
         {pageNumbers.map((number) => (
           <li key={number} className="page-item">
             <a onClick={() => paginate(number)} className="page-link">
@@ -17,6 +30,17 @@ const Pagination = ({ UsersPerPage, totalUsers, paginate }) => {
             </a>
           </li>
         ))}
+        {pageNumbers.includes(currentPage + 1) && (
+          <a
+            className="page-link"
+            onClick={() => {
+              setCurrentPage(currentPage + 1);
+              paginate(currentPage + 1);
+            }}
+          >
+            next
+          </a>
+        )}
       </ul>
     </nav>
   );
