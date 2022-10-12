07068e1f4bcc33ec1f4f6942c3ba935f0351eacc
Meteo-Flagul00 <med.elkadiri48@gmail.com>
2022-10-12 10:17:45 +0100

    closing and open button fonctionallity

--- src/components/pages/client/btnAddClient/BtnAddClient.jsx
+++ src/components/pages/client/btnAddClient/BtnAddClient.jsx
@@ -6,10 +6,22 @@ import RegistrationForm from '../../..'
 import './btnAddCl.css'
 
 function BtnAddClient() {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className='addCl'>
-      <CustomizedDialogs title={"Add Client"}>
-        <RegistrationForm />
+      <CustomizedDialogs title={"Add Client"} open={open} 
+        handleClickOpen={handleClickOpen} 
+        handleClose={handleClose}
+      >
+        <RegistrationForm handleClose={handleClose} />
       </CustomizedDialogs>
     </div>
   )
