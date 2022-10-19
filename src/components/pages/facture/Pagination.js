0ea70be98df953e3baf58bc2c5eb0f08b3d9d637
Meteo-Flagul00 <med.elkadiri48@gmail.com>
2022-10-19 14:20:32 +0100

    facture pagination

---
+++ src/components/pages/facture/Pagination.js
@@ -0,0 +1,51 @@
+import React, {useState} from 'react';
+import { UilStepForward } from '@iconscout/react-unicons'
+import { UilPrevious } from '@iconscout/react-unicons'
+
+const Pagination = ({ FacturesPerPage, totalFactures, paginate }) => {
+  const [currentPage, setCurrentPage] = useState(0)
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalFactures / FacturesPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className='pagination'>
+      {pageNumbers.includes(currentPage - 1) && (
+          <a
+            className="page-link"
+            onClick={() => {
+              setCurrentPage(currentPage - 1);
+              paginate(currentPage - 1);
+            }}
+          >
+            <UilPrevious />
+          </a>
+        )}
+        {pageNumbers.map(number => (
+          <li key={number} className='page-item'>
+            <a onClick={() => paginate(number)} className='page-link'>
+              {number}
+            </a>
+          </li>
+        ))}
+
+        {pageNumbers.includes(currentPage + 1) && (
+          <a
+            className="page-link"
+            onClick={() => {
+              setCurrentPage(currentPage + 1);
+              paginate(currentPage + 1);
+            }}
+          >
+            <UilStepForward/>
+          </a>
+        )}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
